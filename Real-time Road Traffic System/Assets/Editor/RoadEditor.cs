08bc91babff0692c20ad2ea4856ca4f21fe6282a
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2021-02-03 14:15:13 +0000

    Basic road editing implemented

--- Real-time Road Traffic System/Assets/Editor/RoadEditor.cs
+++ Real-time Road Traffic System/Assets/Editor/RoadEditor.cs
@@ -3,22 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Road))]
+[CustomEditor(typeof(Road)), CanEditMultipleObjects]
 public class RoadEditor : Editor
 {
+    Road road;
+    Transform handleTransform;
+    Quaternion handleRotation;
+
     private void OnSceneGUI()
     {
-        Road road = target as Road;
+        road = target as Road;
 
-        Transform handleTransform = road.transform;
-        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+        handleTransform = road.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
             handleTransform.rotation : Quaternion.identity;
         Vector3 point0 = handleTransform.TransformPoint(road.nodeA.Position);
         Vector3 point1 = handleTransform.TransformPoint(road.nodeB.Position);
 
         Handles.color = Color.yellow;
         Handles.DrawLine(point0, point1);
-        Handles.DoPositionHandle(point0, handleRotation);
-        Handles.DoPositionHandle(point1, handleRotation);
+
+        EditorGUI.BeginChangeCheck();
+        point0 = Handles.DoPositionHandle(point0, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(road, "Move Node");
+            EditorUtility.SetDirty(road);
+            road.nodeA.SetPosition(handleTransform.InverseTransformPoint(point0));
+        }
+        EditorGUI.BeginChangeCheck();
+        point1 = Handles.DoPositionHandle(point1, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(road, "Move Node");
+            EditorUtility.SetDirty(road);
+            road.nodeB.SetPosition(handleTransform.InverseTransformPoint(point1));
+        }
     }
 }
--- Real-time Road Traffic System/Assets/Node.cs
+++ Real-time Road Traffic System/Assets/Node.cs
@@ -6,6 +6,11 @@ public class Node : MonoBehaviour
 {
     public Vector3 Position { get { return transform.position; } }
 
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
